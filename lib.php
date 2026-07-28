5870f27acefb3d348ed48200630d0327824b2187
Alistair Spark <aspark21@users.noreply.github.com>
2026-07-28 17:44:59 +0100

    Deprecated in 5.3

--- lib.php
+++ lib.php
@@ -383,8 +383,6 @@ function scheduler_supports($feature) {
             return true;
         case FEATURE_GROUPINGS:
             return true;
-        case FEATURE_GROUPMEMBERSONLY:
-            return true;
         case FEATURE_MOD_INTRO:
             return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS:
